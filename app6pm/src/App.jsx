7681aeb46b847ccb250cf3b39984cacda605e380
Ramroop26 <ramroop26io071@satiengg.in>
2025-08-27 22:00:43 +0530

    commit

--- app6pm/src/App.jsx
+++ app6pm/src/App.jsx
@@ -1,14 +1,9 @@
 // import { useState, createContext } from "react";
-
 // import Cybrom from "./Cybrom";
-
 // import Bhopal from "./Bhopal";
-
 // const MyContext = createContext();
-
 // const App = () => {
 //   const [city, setCity] = useState("Ramroop");
-
 //   return (
 //     <>
 //     <button onClick={()=>setCity("Ujjain")}>click me</button>
@@ -16,17 +11,15 @@
 //       <MyContext.Provider value={{city, setCity}}>
 //         <Bhopal />
 //       </MyContext.Provider>
-
-
 //     </>
 //   );
 // };
-
 // export default App;
 // export{MyContext};
 
-// import Cybrom1 from "./Cybrom1";
 
+
+// import Cybrom1 from "./Cybrom1";
 // const App=()=>{
 //   return(
 //     <>
@@ -36,13 +29,14 @@
 //     </>
 //   )
 // }
-
 // export default App;
+
+
+
 // import Auth from "./Auth";
 // import UnAuth from "./UnAuth";
 // import { useContext} from "react";
 // import { MyLoginContext } from "./LoginContext";
-
 // const App=()=>{
 //   const {user} = useContext(MyLoginContext);
 //   return(
@@ -54,30 +48,25 @@
 // }
 // export default App;
 
-// import { useRef } from "react";
 
+// import { useRef } from "react";
 // const App = () => {
 //   const myRef = useRef();
-
 //   const display1 = () => {
 //     myRef.current.style.fontSize = "30px";
 //     myRef.current.style.borderRadius = "10px";
 //     myRef.current.style.border = "3px solid red";
 //     myRef.current.style.width = "30px";
 //     myRef.current.style.height = "70%";
-
 //   };
-
 //   const display2 = () => {
 //     myRef.current.style.color = "blue";
 //     myRef.current.style.fontWeight = "bold";
 //   };
-
 //   const display3 = () => {
 //     myRef.current.style.backgroundColor = "yellow";
 //     myRef.current.style.padding = "50px";
 //   };
-
 //   return (
 //     <>
 //       <button onClick={display1}>Style 1</button>
@@ -87,21 +76,17 @@
 //     </>
 //   );
 // };
-
 // export default App;
 
 
 // import { useRef, useState, useEffect } from "react"
-
 // const App=()=>{
 //   const [txtval, setTxtVal] = useState("");
 //   useEffect(()=>{
 //     myRef.current=myRef.current+1;
 //   })
-
 //   const myRef = useRef(0);
-//   console.log(myRef.current);
-  
+//   console.log(myRef.current);  
 //   return(
 //     <>
 //     Enter Name: <input type="text" value={txtval}  onChange={(e)=>{setTxtVal(e.target.value)}}/>
@@ -111,8 +96,9 @@
 // }
 // export default App;
 
-// import { useReducer } from "react";
 
+
+// import { useReducer } from "react";
 // const App=()=>{
 //   const myMethod=(state, action)=>{
 //     switch(action){
@@ -122,70 +108,64 @@
 //           return state-1;
 //           default:
 //             return state;
-
 //     }
 //   }
-
 //   const [count, dispatch] = useReducer(myMethod, 0);
 //   return(
 //     <>
 //     <h1>My Counter Program Using useReducer</h1>
 //     <button onClick={()=>{dispatch("Increase")}}>Increment</button>
-
 //     <h1>Count: {count}</h1>
 //     <button onClick={()=>{dispatch("Decrease")}}>Decrement</button>
 //     </>
 //   )
-
 // }
 // export default App;
 
+
 //useMemo used to Memorized value Dalta hai or Performance  
+// import { useState } from "react";
+// import { useMemo } from "react";
+// const App=()=>{
+//   const[add, setAdd] = useState(0);
+//   const [sub, setSub] = useState(100);
+//   const  myMultiVal = useMemo(myMulti, [add]);
+//   function myMulti(){
+//     console.log("*****");
+//     return add*2;
+//   }
+//   return(
+//     <>
+//     <h1>My App</h1>
+//     <h1>My Multiplication : {myMultiVal} </h1>
+//     <button onClick={()=>{setAdd(add+1)}}>Addition</button>
+//     <h2>Addition : {add}</h2>
+//     <button onClick={()=>{setSub(sub-1)}}>Substraction</button>
+//     <h2>Substraction : {sub}</h2>
+//     </>
+//   )
+// }
+// export default App;
+
+
 import { useState } from "react";
 import { useMemo } from "react";
 const App=()=>{
-  const[add, setAdd] = useState(0);
-  const [sub, setSub] = useState(100);
-  const  myMultiVal = useMemo(myMulti, [add]);
-
-  function myMulti(){
-    console.log("*****");
-    return add*2;
+  const [name, setName] = useState("");
+  const [num, setNum] = useState("");
+  const myVal = useMemo(MyFunction, [num]);
+  function MyFunction(){
+    for(var i=0; i<1000000; i++){}
+    return num*2;
   }
-
   return(
     <>
-    <h1>My App</h1>
-    <h1>My Multiplication : {myMultiVal} </h1>
-    <button onClick={()=>{setAdd(add+1)}}>Addition</button>
-    <h2>Addition : {add}</h2>
-    <button onClick={()=>{setSub(sub-1)}}>Substraction</button>
-    <h2>Substraction : {sub}</h2>
+<h1>My App</h1>
+Select Number : <input type="number" value={num} onChange={(e)=>{setNum(e.target.value)}} /> <br /><br />
+Enter Name : <input type="text"  value={name} onChange={(e)=>{setName(e.target.value)}}/> <br /> <br />
+<h2>My Thousand : {myVal}</h2>
     </>
   )
 }
 export default App;
-
-// import { useState } from "react";
-// import { useMemo } from "react";
-
-// const App=()=>{
-//   const [name, setName] = useState("");
-//   const [num, setNum] = useState("");
-//   const myVal = useMemo(MyFunction, [num]);
-//   function MyFunction(){
-//     for(var i=0; i<1000000; i++){}
-//     return num*2;
-//   }
-//   return(
-//     <>
-// <h1>My App</h1>
-// Select Number : <input type="number" value={num} onChange={(e)=>{setNum(e.target.value)}} /> <br /><br />
-// Enter Name : <input type="text"  value={name} onChange={(e)=>{setName(e.target.value)}}/> <br /> <br />
-// <h2>My Thousand : {myVal}</h2>
-//     </>
-//   )
-// }
-
-// export default App;
 

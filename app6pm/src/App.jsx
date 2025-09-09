6fbdf1b49082ab5cc3bcc6d2d8bff0b4199d4a53
Ramroop26 <ramroop26io071@satiengg.in>
2025-09-09 23:26:47 +0530

    commit

--- app6pm/src/App.jsx
+++ app6pm/src/App.jsx
@@ -1,21 +1,21 @@
-// import { useState, createContext } from "react";
-// import Cybrom from "./Cybrom";
-// import Bhopal from "./Bhopal";
-// const MyContext = createContext();
-// const App = () => {
-//   const [city, setCity] = useState("Ramroop");
-//   return (
-//     <>
-//     <button onClick={()=>setCity("Ujjain")}>click me</button>
-//       <h1>welcome to : {city}</h1>
-//       <MyContext.Provider value={{city, setCity}}>
-//         <Bhopal />
-//       </MyContext.Provider>
-//     </>
-//   );
-// };
-// export default App;
-// export{MyContext};
+import { useState, createContext } from "react";
+import Cybrom from "./Cybrom";
+import Bhopal from "./Bhopal";
+const MyContext = createContext();
+const App = () => {
+  const [city, setCity] = useState("Ramroop");
+  return (
+    <>
+    <button onClick={()=>setCity("Ujjain")}>click me</button>
+      <h1>welcome to : {city}</h1>
+      <MyContext.Provider value={{city, setCity}}>
+        <Bhopal />
+      </MyContext.Provider>
+    </>
+  );
+};
+export default App;
+export{MyContext};
 
 
 

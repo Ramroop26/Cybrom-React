57108a2be20795f6d86adf0fc1d0c27053786a7d
Ramroop26 <ramroop26io071@satiengg.in>
2025-09-12 00:01:02 +0530

    commit

--- app6pm/src/App.jsx
+++ app6pm/src/App.jsx
@@ -343,34 +343,34 @@ export{MyContext};
 // export default App;
 
 
-// import { useState } from "react";
-// import axios from "axios";
-// const App=()=>{
-//   const [image, setImage] =  useState("");
+import { useState } from "react";
+import axios from "axios";
+const App=()=>{
+  const [image, setImage] =  useState("");
 
-//   const handleInputImage=(e)=>{
-//     setImage(e.target.files[0]);
+  const handleInputImage=(e)=>{
+    setImage(e.target.files[0]);
 
-//   }
+  }
 
-//   const handleSubmit=async()=>{
-//     const formData = new FormData();
-//    formData.append("file", image);
-//     formData.append("upload_preset", "image1");
-//     const response = await axios.post("https://api.cloudinary.com/v1_1/dkm5xgamv/image/upload", formData);
-//     console.log(response.data);
-//     console.log(response.data.url);
-//     document.getElementById("myimg").src=response.data.url;
+  const handleSubmit=async()=>{
+    const formData = new FormData();
+   formData.append("file", image);
+    formData.append("upload_preset", "image1");
+    const response = await axios.post("https://api.cloudinary.com/v1_1/dkm5xgamv/image/upload", formData);
+    console.log(response.data);
+    console.log(response.data.url);
+    document.getElementById("myimg").src=response.data.url;
 
-//   }
-//   return(
-//     <>
+  }
+  return(
+    <>
 
-// <h1>Upload Image</h1>
-// Upload Image: <input type="file"  onChange={handleInputImage} />
-// <button onClick={handleSubmit}>Upload !</button>
-// <img  id="myimg"/>
-//     </>
-//   )
-// }
-// export default App;
+<h1>Upload Image</h1>
+Upload Image: <input type="file"  onChange={handleInputImage} />
+<button onClick={handleSubmit}>Upload !</button>
+<img  id="myimg"/>
+    </>
+  )
+}
+export default App;
